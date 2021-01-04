d134cfec3d34ddaa937d788faea1fc02ae479fe8
Gonzalo Manrique <gonzalomanriquedeleon@gmail.com>
2021-01-04 00:54:08 -0300

    Add examples to help message

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,24 @@ const program = new Command();
 
 program
   .name("mysql-excel")
-  .version(packageJson.version, "-v --version", "Output version and exit")
-  .helpOption("-H --help", "Display application help")
+  .version(packageJson.version, "-v, --version", "Output version and exit")
+  .helpOption("-H, --help", "Display application help")
   .requiredOption("-h, --host <host>", "Host to connect to", "localhost")
-  .requiredOption("-u --user <user>", "User for login")
+  .requiredOption("-u, --user <user>", "User for login")
   .requiredOption(
-    "-p --password <password>",
+    "-p, --password <password>",
     "Password to use when connecting to the server"
   )
-  .requiredOption("-d --database <database>", "Database to query")
-  .option("-P --port [port]", "Port of the server", "3306")
+  .requiredOption("-d, --database <database>", "Database to query")
+  .option("-P, --port [port]", "Port of the server", "3306")
+  .on('--help', () => {
+    console.log('');
+    console.log('Examples:');
+    console.log('  $ mysql-excel -v');
+    console.log('  $ mysql-excel -H');
+    console.log('  $ mysql-excel -u octocat -p tentacles -d github');
+    console.log('  $ mysql-excel -u octocat -p tentacles -d github -H 127.0.0.1 -P 33066');
+  })
   .action(mysqlExcelAction);
 
 if (process.argv.length === 2) {
